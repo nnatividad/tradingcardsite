bc90e81d96310f28edc7ad45056a301864a83290
nnatividad <k4lani@gmail.com>
2024-09-19 20:34:47 -0700

    filtering and pagination done

--- trading-cards/app/components/table.js
+++ trading-cards/app/components/table.js
@@ -1,18 +1,14 @@
 import React, { useState, useEffect, useMemo } from 'react';
 import './table.css';
 
-function FilterTable(){
-    return(
-        <div className = "filter-container">
-            <h1>Filter Place Holder</h1>
-        </div>
-    );
-}
 
-
-export default function Table(props) {
-    const {cards} = props;
-    const [data, setData] = useState([]);
+export default function Table() {
+    //const {cards} = props;
+    const [data, setData] = useState([]); //useState hook to store data from fetch request
+    //data array of values from fetch request, setData function to update data
+    const [page, setPage] = useState(1);
+    const [filter, setFilter] = useState(' '); //set as empty string (no filters when first rendered)
+    const itemsPerPage = 25;
 
     useEffect(() => {
         fetch('/api/create-151-set')
@@ -21,9 +17,8 @@ export default function Table(props) {
             .catch(error => console.error('Error fetching data:', error));
     }, []);
 
-    const itemsPerPage = 25;
-    const [page, setPage] = useState(1);
-    const displayData = useMemo(() => {
+    const displayData = useMemo(() => {//useMemo hook used to control how many cards show per page
+        if (!data || !Array.isArray(data)) return [];
         const start = (page - 1) * itemsPerPage;
         const end = start + itemsPerPage;
         return data.slice(start, end);
@@ -31,9 +26,74 @@ export default function Table(props) {
 
     const totalPages = Math.ceil(data.length / itemsPerPage);
 
+    function filterData(filter){//function to filter data based on filter value
+        let sortedData = [...data]; //copy of data array
+        console.log(sortedData);
+        if(filter === 'name'){
+            sortedData.sort((a,b) => { //extra null value is caused by null values from other values
+               let nameA;
+               let nameB;
+               if(a.name){
+                    nameA = a.name.toString();
+               }else{
+                     nameA = '';
+               }
+                if(b.name){
+                      nameB = b.name.toString();
+                }else{
+                        nameB = '';
+                }
+                return nameA.localeCompare(nameB);
+            });
+        }else if(filter === 'high-to-low'){//descending order
+            sortedData.sort((a,b) => {
+                let priceA;
+                let priceB;
+                if(a.ungraded_price){
+                    priceA = parseFloat(a.ungraded_price.replace('$', '')); //remove dollar sign
+                }
+                if(b.ungraded_price){
+                    priceB = parseFloat(b.ungraded_price.replace('$', ''));
+                }
+                return priceB - priceA;
+            })
+        }else if (filter === 'low-to-high'){//ascending order
+            sortedData.sort((a,b) => {
+                let priceA;
+                let priceB;
+                if(a.ungraded_price){
+                    priceA = parseFloat(a.ungraded_price.replace('$', ''));
+                }
+                if(b.ungraded_price){
+                    priceB = parseFloat(b.ungraded_price.replace('$', ''));
+                }
+                return priceA - priceB;
+            })
+        }
+        sortedData = sortedData.filter(item => item !== null && item !== undefined);
+        console.log(sortedData);
+        setData(sortedData);
+    }
+
+    const onChange = (event) => { //event handler function for tracking filter changes
+        const value = event.target.value;
+        console.log(value);
+        setFilter(value);
+        filterData(value);
+    };
+
     return (
         <div id = "container">
-            <FilterTable/>
+            <div className = "filter-container">
+                <div className = "drop-down-container">
+                    <label htmlFor ="filters">Filter:</label>
+                    <select id="filters" onChange={onChange}>
+                    <option value='name'>Name: A-Z</option>
+                    <option value='high-to-low'>Price: High to Low</option>
+                    <option value='low-to-high'>Price: Low to High</option>
+                    </select>
+                </div>
+            </div>
             <table className="table">
             <thead>
                 <tr>
